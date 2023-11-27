933f39affaf03b0834f9801f6d556f86e2e30347
Brent Son <brentson00@gmail.com>
2023-11-26 23:49:29 -0800

    updated the login page styles

--- anything-goes-react/src/pages/Login.js
+++ anything-goes-react/src/pages/Login.js
@@ -9,50 +9,55 @@ function Login() {
   const test = "test";
   return (
     <>
-    <main>
-      <div>
-        <h1 style={{color: 'red', fontWeight: 'bold', textAlign: 'center'}}>Returning User:</h1>
-      </div>
-      <div style={{paddingLeft : 350, paddingRight: 350}}>
-        <p>Username:</p>
-      </div>
-      <div style={{paddingLeft : 350, paddingRight: 350}}>
-      <LoginUser children={test}></LoginUser>
-      </div>
-      <div style={{paddingLeft : 350, paddingRight: 350}}>
-        <p>Password:</p>
-      </div>
-      <div  style={{paddingLeft : 350, paddingRight: 350}}>
-      <LoginPass children={test}></LoginPass>
-      </div>
-      <div style={{paddingLeft : 550, paddingRight: 550}}>
-      <Button color="primary">CLICK ME TO LOGIN</Button>
-      </div>
-      <div>
-        <h1 style={{color: 'red', fontWeight: 'bold', textAlign: 'center'}}>New User:</h1>
-      </div>
-      <div style={{paddingLeft : 350, paddingRight: 350}}>
-        <p>New Username:</p>
-      </div>
-      <div style={{paddingLeft : 350, paddingRight: 350}}>
-      <RegUser children={test}></RegUser>
-      </div>
-      <div style={{paddingLeft : 350, paddingRight: 350}}>
-        <p>New Password:</p>
-      </div>
-      <div style={{paddingLeft : 350, paddingRight: 350}}>
-      <RegPass children={test}></RegPass>
-      </div>
-      <div style={{paddingLeft : 350, paddingRight: 350}}>
-        <p>Confirm Password:</p>
-      </div>
-      <div style={{paddingLeft : 350, paddingRight: 350}}>
-      <RegPass children={test}></RegPass>
-      </div>
-      <div style={{paddingLeft : 550, paddingRight: 550}} >
-      <Button color="primary">CLICK ME TO SIGN UP</Button>
-      </div>
-    </main>
+      <main className="container" style={{ justifyContent: "center" }}>
+        <div>
+          <h1 style={{ color: "red", fontWeight: "bold", textAlign: "center" }}>
+            Returning User:
+          </h1>
+        </div>
+        <div style={{ paddingLeft: 350, paddingRight: 350 }}>
+          <p>Username:</p>
+        </div>
+        <div style={{ paddingLeft: 350, paddingRight: 350, marginTop: 25 }}>
+          <LoginUser children={test}></LoginUser>
+        </div>
+        <div style={{ paddingLeft: 350, paddingRight: 350 }}>
+          <p>Password:</p>
+        </div>
+        <div style={{ paddingLeft: 350, paddingRight: 350 }}>
+          <LoginPass children={test}></LoginPass>
+        </div>
+        <div style={{ paddingLeft: 550, paddingRight: 550 }}>
+          <Button color="outline-light">LOGIN</Button>
+        </div>
+        <br />
+        <div>
+          <h1 style={{ color: "red", fontWeight: "bold", textAlign: "center" }}>
+            New User:
+          </h1>
+        </div>
+        <div style={{ paddingLeft: 350, paddingRight: 350 }}>
+          <p>New Username:</p>
+        </div>
+        <div style={{ paddingLeft: 350, paddingRight: 350 }}>
+          <RegUser children={test}></RegUser>
+        </div>
+        <div style={{ paddingLeft: 350, paddingRight: 350 }}>
+          <p>New Password:</p>
+        </div>
+        <div style={{ paddingLeft: 350, paddingRight: 350 }}>
+          <RegPass children={test}></RegPass>
+        </div>
+        <div style={{ paddingLeft: 350, paddingRight: 350 }}>
+          <p>Confirm Password:</p>
+        </div>
+        <div style={{ paddingLeft: 350, paddingRight: 350 }}>
+          <RegPass children={test}></RegPass>
+        </div>
+        <div style={{ paddingLeft: 550, paddingRight: 550 }}>
+          <Button color="outline-light">SIGN UP</Button>
+        </div>
+      </main>
     </>
   );
 }
