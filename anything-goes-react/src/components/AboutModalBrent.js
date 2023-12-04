9f2f03abeba16439043254f977414a8888db362c
andvu860 <litomev@hotmail.com>
2023-12-03 20:47:43 -0800

    added profiles with modals in the about page

---
+++ anything-goes-react/src/components/AboutModalBrent.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const Brent = {
+    "name": "Brent",
+    "description": "I am Brent. One of the developers of Anything Goes. I have a large brain and a larger IQ.",
+    "picture": 'https://cdn.discordapp.com/attachments/1169434139771019347/1180957690101510144/IMG_8059.png?ex=657f4f8e&is=656cda8e&hm=0f60ca5a5c9447822268d85b4fabafb82a7667ccbbcd87450a1fa08696900c84&'
+  }
+
+const AboutModalBrent = ({open,close}) => {
+
+    if(!open) return null
+
+    return (
+
+
+        <div className = 'modal-container' onClick = {close}>
+
+            <div className="modal-text-container">
+
+                <h2 className='modal-admin-name'> {Brent.name} </h2>
+                <center><img src={Brent.picture} style={{width:'250px'}}/></center>
+                <p className='modal-admin-description'>{Brent.description}</p>
+
+            </div>
+        
+
+        </div>
+
+    )
+
+}
+
+export default AboutModalBrent
--- anything-goes-react/src/pages/About.js
+++ anything-goes-react/src/pages/About.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import "../styles.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
@@ -7,8 +7,30 @@ import {
   faPeopleGroup,
   faRobot,
 } from "@fortawesome/free-solid-svg-icons";
+import AboutModalBrent from "../components/AboutModalBrent"
+import AboutModalAaron from "../components/AboutModalAaron"
+import AboutModalBryan from "../components/AboutModalBryan"
+import AboutModalAndrew from "../components/AboutModalAndrew"
+import AboutModalKaia from "../components/AboutModalKaia"
 
-const About = () => {
+const profilePicture = {
+  "Aaron": 'https://media.discordapp.net/attachments/1169434139771019347/1180957688818049084/IMG_8057.png?ex=657f4f8e&is=656cda8e&hm=2da15627e28be3b699750aca194694f9407bb05f735b9e59595d71c978d12bdb&=&width=557&height=670',
+  "Brent": 'https://cdn.discordapp.com/attachments/1169434139771019347/1180957690101510144/IMG_8059.png?ex=657f4f8e&is=656cda8e&hm=0f60ca5a5c9447822268d85b4fabafb82a7667ccbbcd87450a1fa08696900c84&',
+  "Bryan": 'https://media.discordapp.net/attachments/1169434139771019347/1180957689203929198/IMG_8058.png?ex=657f4f8e&is=656cda8e&hm=f971347ba77b2fd09c1a677c1ee2e2651e590221af2f9e01d991fb8cbedd07df&=&width=555&height=670',
+  "Andrew": 'https://cdn.discordapp.com/attachments/1169434139771019347/1180957689623367781/IMG_8060.png?ex=657f4f8e&is=656cda8e&hm=7262dd7f7beac2a541943138b9d50d8628f860326e3ef824dcd2d8ac2d6725e0&',
+  "Kaia": 'https://pbs.twimg.com/media/FzbBULhagAA0SKA?format=jpg&name=4096x4096'
+}
+
+
+
+
+function About() {
+  
+  const [openModalBrent, setOpenModalBrent] = useState(false)
+  const [openModalAaron, setOpenModalAaron] = useState(false)
+  const [openModalBryan, setOpenModalBryan] = useState(false)
+  const [openModalAndrew, setOpenModalAndrew] = useState(false)
+  const [openModalKaia, setOpenModalKaia] = useState(false)
   return (
     <div>
       <section className="container about-page">
@@ -98,6 +120,62 @@ const About = () => {
             </div>
           </div>
         </div>
+
+              <div className='fight-page-images'>
+
+              <div>
+                  <img 
+                  className = "about-modal-image-button"
+                  onClick = {() => setOpenModalBrent(true)}
+                  src={profilePicture.Brent}/>
+                  <AboutModalBrent open = {(openModalBrent)} close = {() => setOpenModalBrent(false)}/>
+              </div>
+
+              <div>
+                  <img 
+                  className = "about-modal-image-button"
+                  onClick = {() => setOpenModalAaron(true)}
+                  src={profilePicture.Aaron}/>
+                  <AboutModalAaron open = {(openModalAaron)} close = {() => setOpenModalAaron(false)}/>
+              </div>
+
+              <div>
+                  <img 
+                  className = "about-modal-image-button"
+                  onClick = {() => setOpenModalBryan(true)}
+                  src={profilePicture.Bryan}/>
+                  <AboutModalBryan open = {(openModalBryan)} close = {() => setOpenModalBryan(false)}/>
+              </div>
+
+              <div>
+                  <img 
+                  className = "about-modal-image-button"
+                  onClick = {() => setOpenModalAndrew(true)}
+                  src={profilePicture.Andrew}/>
+                  <AboutModalAndrew open = {(openModalAndrew)} close = {() => setOpenModalAndrew(false)}/>
+              </div>
+
+              <div>
+                  <img 
+                  className = "about-modal-image-button"
+                  onClick = {() => setOpenModalKaia(true)}
+                  src={profilePicture.Kaia}/>
+                  <AboutModalKaia open = {(openModalKaia)} close = {() => setOpenModalKaia(false)}/>
+              </div>
+
+
+
+
+
+
+
+              </div>
+
+
+
+
+
+
       </section>
     </div>
   );
