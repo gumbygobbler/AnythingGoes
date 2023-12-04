2363160a86c0c392b7508fc204268cc79b7223ee
Dade Fang <fangdade@gmail.com>
2023-12-04 04:09:03 -0800

    Added complexity to the add fight functionality

--- django-backend/backend/views.py
+++ django-backend/backend/views.py
@@ -1,9 +1,52 @@
 from django.http import JsonResponse
 from .models import fighter
+from .models import manager
 from .serializers import fighterSerializer
+from .serializers import managerSerializer
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+
+@api_view(['GET', 'POST'])
+def manager_list(request, format=None, *args, **kwargs):
+    #get all the drinks
+    #serialize them
+    #return json
+    if request.method == 'GET':
+        managers = manager.objects.all() #grabs all fighter objects
+        serializer = managerSerializer(managers, many=True)
+        return Response(serializer.data)
+
+    if request.method == 'POST':
+        serializer = managerSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            #print("here" + serializer.data)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def manager_detail(request, userID, format=None):
+
+    try:
+        managerData = manager.objects.get(userID = userID)
+    except manager.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    if request.method == 'GET':
+        serializer = managerSerializer(managerData)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = managerSerializer(managerData, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    elif request.method == 'DELETE':
+        managerData.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
 
 @api_view(['GET', 'POST'])
 def fighter_list(request, format=None, *args, **kwargs):
@@ -19,6 +62,7 @@ def fighter_list(request, format=None, *args, **kwargs):
         serializer = fighterSerializer(data = request.data)
         if serializer.is_valid():
             serializer.save()
+            #print("here" + serializer.data)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
     # if request.method =='POST':
