07734f6e0c46e1ee881c7e13c3b33ee9177f0241
Kaia Ralston <kralston1127@sdsu.edu>
2023-12-10 12:23:03 -0800

    logo animation done. Starting to work on other styling now

--- anything-goes-react/src/pages/logo.js
+++ anything-goes-react/src/pages/logo.js
@@ -5,19 +5,33 @@ import b from './logoImages/2.jpg';
 import c from './logoImages/3.jpg';
 import d from './logoImages/4.png';
 import e from './logoImages/5.jpg';
-import kirby from './logoImages/kirby.png';
+import f from './logoImages/6.png';
+import g from './logoImages/7.png';
+import h from './logoImages/8.png';
+import i from './logoImages/9.png';
+import j from './logoImages/10.png';
+import k from './logoImages/11.png';
+import l from './logoImages/12.jpg';
+import m from './logoImages/13.png';
+import n from './logoImages/14.jpg';
+import o from './logoImages/15.png';
+import p from './logoImages/16.png';
+import q from './logoImages/17.png';
+import r from './logoImages/18.png';
+import s from './logoImages/19.png';
+import t from './logoImages/20.png';
+import u from './logoImages/21.png';
+import v from './logoImages/22.png';
+import w from './logoImages/23.png';
+import x from './logoImages/24.png';
+import y from './logoImages/25.png';
 import { useState } from 'react';
 
 
 
 const Logo = () => {
   const images = [
-    a,
-    b,
-    c,
-    d,
-    e,
-    kirby
+    a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y
   ];
 
   const [currentLeftImage, setcurrentLeftImage] = useState(images[0]);
@@ -50,7 +64,7 @@ const Logo = () => {
       </div>
 
       <div>
-        <button className = "backToHomeBtn" onClick={handleChangeImage}>Change Image</button>
+        <button className = "newFightLogoBtn" onClick={handleChangeImage}>Regenerate Random Fight</button>
       </div>
 
       <div className="rightFight">
