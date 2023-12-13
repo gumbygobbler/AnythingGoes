841e203ab33fe4f9026d02ef21e3a3bc63304daa
Brent Son <brentson00@gmail.com>
2023-12-13 00:57:59 -0800

    debugged final build to successfully run properly as it is

--- anything-goes-react/src/components/NavBar.js
+++ anything-goes-react/src/components/NavBar.js
@@ -4,55 +4,52 @@ import "../styles.css";
 import AuthContext from "../context/AuthContext";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
+import LogoutModal from "./LogoutModal";
+
 import {
   faCircleUser,
   faHouse,
-  faInfoCircle
+  faInfoCircle,
 } from "@fortawesome/free-solid-svg-icons";
-
 
 const NavBar = () => {
   let { user, logoutUser } = useContext(AuthContext);
   const [openModalLogout, setOpenModalLogout] = useState(false);
 
   return (
-    
     <div>
-      <LogoutModal open = {openModalLogout}/>
+      <LogoutModal open={openModalLogout} />
       <nav>
         <ButtonLink to="/home" classes="navbar-button">
-        <FontAwesomeIcon
-              icon={faHouse}
-              style={{
-                fontSize: "20px",
-                marginRight: "1px",
-                padding: "3px",
-                paddingTop: "0px",
-                color: "rgb(130, 39, 163)"
-              }}
-            />
+          <FontAwesomeIcon
+            icon={faHouse}
+            style={{
+              fontSize: "20px",
+              marginRight: "1px",
+              padding: "3px",
+              paddingTop: "0px",
+              color: "rgb(130, 39, 163)",
+            }}
+          />
           Home
         </ButtonLink>
         <ButtonLink to="/about" classes="navbar-button">
-        <FontAwesomeIcon
-              icon={faInfoCircle}
-              style={{
-                fontSize: "20px",
-                marginRight: "1px",
-                padding: "3px",
-                color: "rgb(130, 39, 163)",
-              }}
-            />
+          <FontAwesomeIcon
+            icon={faInfoCircle}
+            style={{
+              fontSize: "20px",
+              marginRight: "1px",
+              padding: "3px",
+              color: "rgb(130, 39, 163)",
+            }}
+          />
           About
         </ButtonLink>
         <br />
         {user ? (
           <>
             <p>User: {user.username}</p>
-            <button
-              className="navbar-button"
-              onClick={logoutUser}
-            >
+            <button className="navbar-button" onClick={logoutUser}>
               Logout
             </button>
           </>
--- anything-goes-react/src/pages/Signup.js
+++ anything-goes-react/src/pages/Signup.js
@@ -3,7 +3,6 @@ import React, { useEffect, useState, useContext } from "react";
 import FormData from "form-data";
 import "./pages.css";
 
-
 //import AuthContext from "../context/AuthContext";
 import { jwtDecode } from "jwt-decode";
 
@@ -38,12 +37,11 @@ function Signup() {
 
   function handleSubmit(event) {
     event.preventDefault();
-    
-   const newUserData = new FormData()
-    newUserData.append('username', values.name)
-    newUserData.append('password', values.password)
-    newUserData.append('password2', values.password2)
 
+    const newUserData = new FormData();
+    newUserData.append("username", values.name);
+    newUserData.append("password", values.password);
+    newUserData.append("password2", values.password2);
 
     setErrors(Validation(values, 2));
 
@@ -142,32 +140,28 @@ function Signup() {
 
   return (
     <div style={{ height: "100vh" }}>
-
-      <form onSubmit={HandleSubmit}>
-      <div className="container border border-white login-box d-flex flex-column justify-content-center mt-5 w-50 text-white ">
-          
-      <div className=" d-flex flex-column justify-content-center">
+      <form onSubmit={handleSubmit}>
+        <div className="container border border-white login-box d-flex flex-column justify-content-center mt-5 w-50 text-white ">
+          <div className=" d-flex flex-column justify-content-center">
             <br></br>
-            
+
             <label
               style={{
                 color: "#c9a7d5",
                 textAlign: "center",
                 fontSize: "60px",
                 justifyContent: "center",
-                fontWeight: "bold", 
-                textShadow: "0px 2px 6px rgba(179, 147, 211, 0.8)"
-                
+                fontWeight: "bold",
+                textShadow: "0px 2px 6px rgba(179, 147, 211, 0.8)",
               }}
             >
-              
               Anything Goes Registration
             </label>
             <br />
             <br />
             <br></br>
             <input
-              className="bgfield"          
+              className="bgfield"
               type="text"
               placeholder="Enter Username"
               name="username"
@@ -179,9 +173,8 @@ function Signup() {
               <p style={{ color: "#ff3333" }}>{errors.username}</p>
             )}
           </div>
-          <div style={{height: "4px"}}></div>
+          <div style={{ height: "4px" }}></div>
           <div className=" d-flex flex-column justify-content-center">
-          
             {/* <label
               className="text-center"
               style={{
@@ -203,7 +196,7 @@ function Signup() {
             {errors.password && (
               <p style={{ color: "#ff3333" }}>{errors.password}</p>
             )}
-            <div style={{height: "4px"}}></div>
+            <div style={{ height: "4px" }}></div>
             <input
               className="bgfield"
               type="password"
@@ -218,25 +211,38 @@ function Signup() {
             )}
           </div>
           <div className="d-flex justify-content-center">
-          <Button style={{padding: "15px", 
-                          color: "#c9a7d5", 
-                          fontWeight: "bold", 
-                          fontSize: "20px",
-                          textShadow: "0px 1px 3px rgba(179, 147, 211, 0.8)"}} 
-                          type="submit" 
-                          variant="outline-light" 
-                          className="my-3 w-100">
+            <Button
+              style={{
+                padding: "15px",
+                color: "#c9a7d5",
+                fontWeight: "bold",
+                fontSize: "20px",
+                textShadow: "0px 1px 3px rgba(179, 147, 211, 0.8)",
+              }}
+              type="submit"
+              variant="outline-light"
+              className="my-3 w-100"
+            >
               Sign Up
             </Button>
           </div>
           {/* //ButtonLink to="/home" classes="general-button"Login/ButtonLink */}
-              
-          <br>
-          </br>
+
+          <br></br>
           <br />
 
-          <span style={{textAlign: "center", color: "lightgray"}}>
-            Returning User? <a style={{color: "#c9a7d5", textShadow: "0px 1px 3px rgba(179, 147, 211, 0.8)"}} href="/Login">Login!</a></span>
+          <span style={{ textAlign: "center", color: "lightgray" }}>
+            Returning User?{" "}
+            <a
+              style={{
+                color: "#c9a7d5",
+                textShadow: "0px 1px 3px rgba(179, 147, 211, 0.8)",
+              }}
+              href="/Login"
+            >
+              Login!
+            </a>
+          </span>
         </div>
       </form>
     </div>
